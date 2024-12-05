36f3058b4b635363426a955e3104ec978f2e3fb0
Lighdx <lapared.com@gmail.com>
2024-12-04 22:03:15 -0400

    asd

--- src/assets/js/panels/home.js
+++ src/assets/js/panels/home.js
@@ -336,7 +336,7 @@ class Home {
         let year = date.getFullYear()
         let month = date.getMonth() + 1
         let day = date.getDate()
-        let allMonth = ['Enero', 'Febrero', 'Marzo', 'Abril', 'mai', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
+        let allMonth = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'juin', 'juillet', 'août', 'septembre', 'octobre', 'novembre', 'décembre']
         return { year: year, month: allMonth[month - 1], day: day }
     }
 }
